546c52b5f5121fbbd6d23e7100d24f8cbc94a0c5
Nikita Tsyhanenko <runninzero@gmail.com>
2015-11-22 16:53:14 +0300

    init demo env

---
+++ demo/server.js
@@ -0,0 +1,27 @@
+﻿var http = require('http'),
+    fs = require('fs');
+
+http
+    .createServer(function (request, response) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+
+        var filename = request.url;
+
+        if (filename === '/favicon.ico') {
+            response.end();
+            return;
+        }
+        if (filename === '/') filename = 'index.html';
+        else filename = '..' + filename;
+
+        fs.readFile(filename, function (error, filecontent) {
+                if (error) throw error;
+
+                response.write(filecontent);
+                response.end();
+            }
+        );
+    })
+    .listen(2000, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:2000/');
