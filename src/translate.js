fe6792094982d52eb0b31a93248509b92395b1ef
Nikita Tsyhanenko <runninzero@gmail.com>
2015-11-22 17:40:07 +0300

    implement get and set methods for storage

--- src/translate.js
+++ src/translate.js
@@ -1,5 +1,40 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('mutant-ng-translate', []);
+    angular.module('mutant-ng-translate', [])
+        .factory('$translateStorage', [translateStorage]);
+
+    function translateStorage() {
+        var self = this;
+
+        // DATA
+        self.langs = {};
+
+        // GET
+        self.getValues = function (lang) {
+            if (self.langs.hasOwnProperty(lang)) return self.langs[lang];
+            else return null;
+        };
+
+        self.getValue = function(lang, key) {
+            if (self.langs.hasOwnProperty(lang)
+                && self.lang[lang].hasOwnProperty(key)) return self.langs[lang][key];
+            else return null;
+        };
+
+        // SET
+        self.setValues = function (lang, values) {
+            if (!self.langs.hasOwnProperty(lang)) {
+                self.langs[lang] = {};
+            }
+
+            for (var key in values) {
+                if (!values.hasOwnProperty(key)) continue;
+
+                self.langs[lang][key] = values[key];
+            }
+        };
+
+        return self;
+    };
 })();
