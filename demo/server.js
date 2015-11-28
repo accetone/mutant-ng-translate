7bfa051c30b1147fce45bad0d9b0c80dbeeb8831
Nikita Tsyhanenko <runninzero@gmail.com>
2015-11-28 18:35:44 +0300

    modify demo server

--- demo/server.js
+++ demo/server.js
@@ -9,24 +9,40 @@ http
             return;
         }
 
-        var filename = request.url;
-        if (filename === '/') filename = 'index.html';
+        var filename;
+        if (request.url === '/') filename = 'index.html';
+        else filename = '.' + request.url;
 
-        try {
-            var filecontent = fs.readFileSync(filename);
+        if (filename.indexOf('second') !== -1) {
+            try {
+                setTimeout(function() {
+                    var filecontent = fs.readFileSync(filename);
 
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(filecontent);
-            response.end();
-        } catch (e) {}
+                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                    response.write(filecontent);
+                    response.end();
+                }, 5000);
+            } catch (e) {
+            }
+        } else {
+            try {
+                var filecontent = fs.readFileSync(filename);
 
-        try {
-            var filecontent = fs.readFileSync('..' + filename);
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.write(filecontent);
+                response.end();
+            } catch (e) { }
 
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(filecontent);
-            response.end();
-        } catch (e) {}
+            filename = '..' + request.url;
+
+            try {
+                var filecontent = fs.readFileSync(filename);
+
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.write(filecontent);
+                response.end();
+            } catch (e) { }
+        }
     })
     .listen(2000, '127.0.0.1');
 
