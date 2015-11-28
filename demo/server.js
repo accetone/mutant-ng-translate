a8f1f7c96838253d262fcfd1ef82892bbbbd892c
Nikita Tsyhanenko <runninzero@gmail.com>
2015-11-28 19:15:55 +0300

    refactor demo env server

--- demo/server.js
+++ demo/server.js
@@ -1,48 +1,54 @@
 ﻿var http = require('http'),
     fs = require('fs');
 
+var send404 = function(response) {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end();
+};
+
+var sendFile = function(response, file) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(file);
+    response.end();
+};
+
+var sendFileWithDelay = function(response, file, delay) {
+    setTimeout(function() {
+        sendFile(response, file);
+    }, delay);
+};
+
+var transformUrlToPath = function(url) {
+    return url.replace(/\//g, '\\').substring(1);
+};
+
+var getFile = function(url) {
+    var path = transformUrlToPath(url);
+
+    if (path === '') path = 'index.html';
+
+    if (!fs.existsSync(path)) {
+        path = '..\\' + path;
+
+        if (!fs.existsSync(path)) {
+            return null;
+        }
+    }
+
+    console.log(path);
+
+    return fs.readFileSync(path);
+};
+
 http
-    .createServer(function(request, response) {
-        if (request.url === '/favicon.ico') {
-            response.writeHead(404, { 'Content-Type': 'text/html' });
-            response.end();
-            return;
+    .createServer(function (request, response) {
+        var file = getFile(request.url);
+
+        if (file) {
+            if (request.url.indexOf('second') !== -1) sendFileWithDelay(response, file, 5000);
+            else sendFile(response, file);
         }
-
-        var filename;
-        if (request.url === '/') filename = 'index.html';
-        else filename = '.' + request.url;
-
-        if (filename.indexOf('second') !== -1) {
-            try {
-                setTimeout(function() {
-                    var filecontent = fs.readFileSync(filename);
-
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.write(filecontent);
-                    response.end();
-                }, 5000);
-            } catch (e) {
-            }
-        } else {
-            try {
-                var filecontent = fs.readFileSync(filename);
-
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.write(filecontent);
-                response.end();
-            } catch (e) { }
-
-            filename = '..' + request.url;
-
-            try {
-                var filecontent = fs.readFileSync(filename);
-
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.write(filecontent);
-                response.end();
-            } catch (e) { }
-        }
+        else send404(response);
     })
     .listen(2000, '127.0.0.1');
 
