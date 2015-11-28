4aa626e5603390411b4e509e4eed0051f55c9e50
Nikita Tsyhanenko <runninzero@gmail.com>
2015-11-28 21:42:18 +0300

    refactor translate storage and loader

--- src/translate.js
+++ src/translate.js
@@ -10,57 +10,59 @@
     function translateStorage() {
         var self = this;
 
-        // DATA
         self.langs = {};
 
-        // GET
-        self.getValues = function (lang) {
+        self.getValues = getValues;
+        self.getValue = getValue;
+        self.setValues = setValues;
+
+        return self;
+
+        function getValues(lang) {
             if (self.langs.hasOwnProperty(lang)) return self.langs[lang];
             else return null;
-        };
+        }
 
-        self.getValue = function(lang, key) {
+        function getValue(lang, key) {
             if (self.langs.hasOwnProperty(lang)
                 && self.lang[lang].hasOwnProperty(key)) return self.langs[lang][key];
             else return null;
-        };
+        }
 
-        // SET
-        self.setValues = function (lang, values) {
+        function setValues(lang, values) {
             if (!self.langs.hasOwnProperty(lang)) {
                 self.langs[lang] = {};
             }
 
             angular.extend(self.langs[lang], values);
-        };
-
-        return self;
+        }
     };
 
     function translateLoader($http, $q) {
         var self = this;
         
-        // LOAD
-        self.loadPart = function(options) {
+        self.loadPart = loadPart;
+
+        return self;
+
+        function loadPart(options) {
             var url = options
                 .urlTemplate
                 .replace(/{part}/g, options.part)
                 .replace(/{lang}/g, options.lang);
 
-            return $q(function(resolve, reject) {
+            return $q(function (resolve, reject) {
                 $http
                     .get(url)
                     .then(function (response) {
                         var values = options.dataTransformation(response.data);
                         resolve(values);
                     })
-                    .catch(function(error) {
+                    .catch(function (error) {
                         reject(error);
                     });
             });
-        };
-
-        return self;
+        }
     };
 
     function translateCache($window) {
