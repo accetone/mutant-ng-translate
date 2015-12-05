a1cc862104c3fca9479bae75578bc46cb58c87b0
Nikita Tsyhanenko <runninzero@gmail.com>
2015-12-05 23:08:11 +0300

    implement filter basic functionality

---
+++ src/filter/filter.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('mutant-ng-translate')
+        .filter('translate', ['$translate', translateFilter]);
+
+    function translateFilter($translate) {
+        var filter = function(key) {
+            return $translate.translation(key);
+        };
+
+        filter.$stateful = true;
+
+        return filter;
+    };
+})();
