3a1fe205458e3c95e0aefb152cce8f5cf6d28b38
Nikita Tsyhanenko <runninzero@gmail.com>
2015-12-05 22:27:17 +0300

    update gulp to watch js changes and build library

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-gulp.task('default', function () {
+gulp.task('build', function () {
     return gulp
         .src('./src/**/*.js')
         .pipe(concat('mutant-ng-translate.js'))
         .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./src/**/*.js', ['build']);
 });
