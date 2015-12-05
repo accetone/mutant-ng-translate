e1a52b1477be504c8ea9b91b076558851ed33e2b
Nikita Tsyhanenko <runninzero@gmail.com>
2015-12-05 23:08:26 +0300

    implement directive basic functionality

---
+++ src/directive/directive.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('mutant-ng-translate')
+        .directive('ngTranslate', ['$translate', '$rootScope', translateDirective]);
+
+    function translateDirective($translate, $rootScope) {
+        var link = function (scope, element, attrs) {
+            element = element[0];
+
+            var translitionKey = attrs['ngTranslate'];
+
+            $rootScope.$on('translationsUpdated', function () {
+                element.innerHTML = $translate.translation(translitionKey);
+            });
+
+            element.innerHTML = $translate.translation(translitionKey);
+        };
+
+        return {
+            restrict: 'A',
+            link: link
+        };
+    };
+})();
