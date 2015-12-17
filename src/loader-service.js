172fd15f9982aac57bae675c3f1e93e7ee58a819
Nikita Tsyhanenko <runninzero@gmail.com>
2015-12-17 20:59:18 +0300

    implement loader service to move load/preload parts logic to separate module from service (not used yet)

---
+++ src/loader-service.js
@@ -0,0 +1,152 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('mutant-ng-translate')
+        .factory('$translateLoaderSvc', ['$translateLoader', '$translateEvents', translateLoaderSvc]);
+
+    function translateLoaderSvc($translateLoader, $translateEvents) {
+        var self = this;
+
+        self.config = config;
+        self.options = undefined;
+
+        self.parts = [];
+
+        self.addParts = addParts;
+        self.addPart = addPart;
+
+        self.loadParts = loadParts;
+        self.loadPart = loadPart;
+
+        self.sync = {
+            counter: {},
+            loading: {},
+            loaded: {},
+
+            loadingOn: loadingOn,
+            loadingOff: loadingOff,
+            isLoading: isLoading,
+            isLoaded: isLoaded,
+            needLoad: needLoad,
+
+            increaseCounter: increaseCounter,
+            resetCounter: resetCounter,
+            checkCounter: checkCounter
+        };
+
+        self.preload = preload;
+
+        return self;
+
+        /* CONFIG */
+        function config(options) {
+            self.options = options;
+
+            $translateEvents.allPartsLoaded.subscribe(self.preload, true);
+        }
+
+        /* PARTS */
+        function addParts(names, lang) {
+            for (var i = 0; i < names.length; i++) {
+                self.addPart(names[i], lang);
+            }
+        }
+
+        function addPart(name, lang) {
+            // TODO: check if part with this name already added
+            if (!name) return;
+
+            var part = { name: name };
+            self.parts.push(part);
+
+            self.loadPart(part, lang);
+        }
+        
+        function loadParts(lang, force) {
+            if (force) {
+                self.sync.resetCounter(lang);
+            }
+
+            for (var i = 0; i < self.parts.length; i++) {
+                if (!force && !self.needLoad(self.parts[i], lang)) continue;
+
+                self.parts.load(self.parts[i], lang);
+            }
+        }
+
+        function loadPart(part, lang) {
+            self.sync.loadingOn(part, lang);
+
+            var partOptions = {
+                part: part,
+                lang: lang,
+                urlTemplate: self.options.urlTemplate,
+                dataTransformation: self.options.dataTransformation
+            };
+
+            return $q(function (resolve, reject) {
+                $translateLoader
+                    .loadPart(partOptions)
+                    .then(function(values) {
+                        self.sync.loadingOff(part, lang);
+                        self.sync.increaseCounter(lang);
+                        self.sync.checkCounter(lang);
+
+                        resolve(values);
+                    })
+                    .catch(function(error) {
+                        reject(error);
+                    });
+            });
+        }
+
+        /* SYNCHRONIZATION */
+        function loadingOn(part, lang) {
+            self.sync.loading[lang + '.' + part.name] = true;
+        }
+
+        function loadingOff(part, lang) {
+            self.sync.loading[lang + '.' + part.name] = false;
+            self.sync.loaded[lang + '.' + part.name] = true;
+        }
+
+        function isLoading(part, lang) {
+            return self.sync.loading[lang + '.' + part.name];
+        }
+
+        function isLoaded(part, lang) {
+            return self.sync.loaded[lang + '.' + part.name];
+        }
+
+        function increaseCounter(lang) {
+            if (!self.sync.counter[lang]) self.sync.counter[lang] = 1;
+            else self.sync.counter[lang]++;
+        }
+
+        function resetCounter(lang) {
+            self.sync.counter[lang] = 0;
+        }
+
+        function checkCounter(lang) {
+            if (self.sync.counter[lang] !== self.parts.length) return;
+
+            $translateEvents.allPartsLoaded.publish(lang);
+        }
+
+        function needLoad(part, lang) {
+            return !isLoading(part, lang) && !isLoaded(part, lang);
+        }
+
+        /* PRELOAD */
+        function preload() {
+            if (!self.options.preloadLanguages) return;
+
+            for (var i = 0; i < self.options.preloadLanguages.length; i++) {
+                var lang = self.options.preloadLanguages[i];
+
+                self.loadParts(lang);
+            }
+        }
+    }
+})();
