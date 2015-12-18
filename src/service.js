94cfe86f7404fbaed5d7810ffdbb03e97e3fc0cf
Nikita Tsyhanenko <runninzero@gmail.com>
2015-12-18 18:57:37 +0300

    update main service to use loader and storage services

--- src/service.js
+++ src/service.js
@@ -3,54 +3,31 @@
 
     angular
         .module('mutant-ng-translate')
-        .factory('$translate', ['$translateUtils', '$translateStorage', '$translateLoader', '$translateCache', '$translateEvents', translate]);
+        .factory('$translate', ['$translateUtils', '$translateStorageSvc', '$translateLoaderSvc', translate]);
 
-    function translate($tranlslateUtils, $translateStorage, $translateLoader, $translateCache, $translateEvents) {
+    function translate($tranlslateUtils, $storage, $loader) {
         var self = this;
 
         self.options = {
             dataTransformation: $tranlslateUtils.directDataTransformation,
             cacheTranslations: true,
-            cacheSelectedLang: true
+            cacheSelectedLang: true,
+            preloadLanguages: []
         };
         
         self.config = config;
         self.validateOptions = validateOptions;
-        self.initSubscriptions = initSubscriptions;
-
-        self.cache = {
-            save: {
-                language: cacheSaveLanguage,
-                translations: cacheSaveTranslations
-            },
-            load: {
-                language: cacheLoadLanguage,
-                translations: cacheLoadTranslations
-            }
-        };
-
+     
         self.use = use;
-        self.langKey = langKey;
+        self.current = currentLang;
 
         self.translations = translations;
         self.translation = translation;
 
-        self.parts = {
-            list: [],
+        self.addPart = addPart;
+        self.addParts = addParts;
 
-            add: addParts,
-            load: loadPart,
-            
-            refresh: refresh,
-
-            loadedCounter: 0
-        };
-
-        self.callbacks = {
-            preload: preloadCallback,
-            partLoaded: partLoadedCallback,
-            languageChanged: languageChangedCallback
-        };
+        self.refresh = refresh;
 
         return self;
         
@@ -59,167 +36,62 @@
             angular.extend(self.options, options);
             self.validateOptions();
 
-            self.cache.load.language();
-            self.cache.load.translations();
-
-            self.initSubscriptions();
+            $storage.config(self.options);
+            $loader.config(self.options);
         }
 
         function validateOptions() {
             $tranlslateUtils.validateOptions(self.options);
         }
 
-        function initSubscriptions() {
-            $translateEvents.partLoaded.subscribe(self.callbacks.partLoaded);
-            $translateEvents.languageChanged.subscribe(self.callbacks.languageChanged);
-            $translateEvents.allPartsLoaded.subscribe(self.callbacks.preload, true);
-        }
-
-        /* CACHE */
-        function cacheLoadLanguage() {
-            if (self.options.cacheSelectedLang) {
-                self.options.lang = $translateCache.getLang();
-
-                if (!self.options.lang) {
-                    self.options.lang = self.options.defaultLang;
-                    $translateCache.setLang(self.options.defaultLang);
-                }
-            } else {
-                self.options.lang = self.options.defaultLang;
-            }
-        }
-
-        function cacheSaveLanguage(lang) {
-            if (!self.options.cacheSelectedLang) return;
-
-            $translateCache.setLang(lang);
-        }
-
-        function cacheLoadTranslations() {
-            if (!self.options.cacheTranslations) return;
-
-            var cacheValues = $translateCache.getValues(self.options.lang);
-            $translateStorage.setValues(self.options.lang, cacheValues);
-        }
-
-        function cacheSaveTranslations(lang, values) {
-            if (!self.options.cacheTranslations) return;
-
-            $translateCache.setValues(lang, values);
-        }
-
         /* LANGS */
         function use(lang) {
-            if (self.options.lang === lang) return;
+            if ($storage.getLanguage() === lang) return;
 
-            self.options.lang = lang;
-
-            self.cache.save.language(lang);
-            self.cache.load.translations();
-
-            self.parts.refresh();
-
-            $translateEvents.languageChanged.publish();
+            $storage.setLanguage(lang);
+            $loader.loadParts(lang, $storage.setTranslations, false);
         }
 
-        function langKey() {
-            return self.options.lang;
+        function currentLang() {
+            return $storage.getLanguage();
         }
 
         /* TRANSLATIONS */
         function translations(lang, values) {
-            if (!!values) {
-                $translateStorage.setValues(lang, values);
+            if (!lang) {
+                lang = $storage.getLanguage();
             }
 
-            if (!!lang) return $translateStorage.getValues(lang);
-                
-            return $translateStorage.getValues(self.options.lang);
+            if (!!values) {
+                $storage.setTranslations(lang, values);
+            }
+
+            return $storage.getTranslations(lang);
         }
 
         function translation(key) {
-            return $translateStorage.getValue(self.options.lang, key);
+            var lang = $storage.getLanguage();
+
+            return $storage.getTranslation(lang, key);
         } 
 
         /* PARTS */
         function addParts() {
-            for (var i = 0; i < arguments.length; i++) {
-                addPart(arguments[i]);
-            }
+            var lang = $storage.getLanguage();
+
+            $loader.addParts(arguments, lang, $storage.setTranslations);
         }
 
         function addPart(name) {
-            if (!name) return;
+            var lang = $storage.getLanguage();
 
-            var partOptions = {
-                part: { name: name },
-                lang: self.options.lang,
-                urlTemplate: self.options.urlTemplate,
-                dataTransformation: self.options.dataTransformation
-            };
-
-            self.parts.list.push(partOptions.part);
-
-            self.parts.load(partOptions);
-        }
-
-        function loadPart(partOptions) {
-            $translateLoader
-                .loadPart(partOptions)
-                .then(function (values) {
-                    partOptions.part[partOptions.lang] = true;
-                    
-                    $translateStorage.setValues(partOptions.lang, values);
-                    self.cache.save.translations(partOptions.lang, values);
-                    
-                    $translateEvents.partLoaded.publish(partOptions);
-                });
-        }
-
-        function loadParts(lang, force) {
-            for (var i = 0; i < self.parts.list.length; i++) {
-                if (self.parts.list[i][lang] && !force) continue;
-
-                var partOptions = {
-                    part: self.parts.list[i],
-                    lang: lang,
-                    urlTemplate: self.options.urlTemplate,
-                    dataTransformation: self.options.dataTransformation
-                };
-
-                self.parts.load(partOptions);
-            }
+            $loader.addPart(name, lang, $storage.setTranslations);
         }
 
         function refresh(force) {
-            var lang = self.options.lang;
+            var lang = $storage.getLanguage();
 
-            loadParts(lang, force);
-        }
-
-        /* CALLACKS */
-        function partLoadedCallback(partOptions) {
-            if (partOptions.lang !== self.options.lang) return;
-
-            self.parts.loadedCounter++;
-
-            if (self.parts.loadedCounter === self.parts.list.length) {
-                $translateEvents.allPartsLoaded.publish(partOptions.lang);
-            }
-        }
-
-        function languageChangedCallback() {
-            self.parts.loadedCounter = 0;
-        }
-
-        function preloadCallback() {
-            if (!self.options.preloadLanguages) return;
-
-            for (var i = 0; i < self.options.preloadLanguages.length; i++) {
-                var lang = self.options.preloadLanguages[i];
-
-                loadParts(lang);
-            }
+            $loader.loadParts(lang, $storage.setTranslations, force);
         }
     };
 })();
