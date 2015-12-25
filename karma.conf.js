9b26f78a6ba9fa01e766b8e1a3a21b9b4c7b8458
Nikita Tsyhanenko <runninzero@gmail.com>
2015-12-26 00:11:07 +0300

    add karma config file

---
+++ karma.conf.js
@@ -0,0 +1,18 @@
+﻿module.exports = function (config) {
+    config.set({
+        frameworks: ['jasmine'],
+
+        files: [
+          'bower_components/angular/angular.js',
+          'bower_components/angular-mocks/angular-mocks.js',
+          'src/translate.js',
+          'src/*.js',
+          'tests/*.js'
+        ],
+
+        reporters: ['progress'],        
+
+        port: 2001,
+        browsers: ['Chrome']
+    });
+};
