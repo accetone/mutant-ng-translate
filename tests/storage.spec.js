5bb5bb3927792eb7532cd16b736eab398c32b5c8
Nikita Tsyhanenko <runninzero@gmail.com>
2015-12-29 00:23:15 +0300

    implement get/set tests for translate storage suite

--- tests/storage.spec.js
+++ tests/storage.spec.js
@@ -57,5 +57,71 @@ describe('The translate storage test suite', function () {
         });
     });
 
+
+    describe('Get/set tests', function() {
+        it('should return an empty object if called without lang', function () {
+            var values = $translateStorage.getValues();
+
+            expect(values).toEqual({});
+        });
+
+        it('should return an empty object if called before set', function () {
+            var values = $translateStorage.getValues(en.key);
+
+            expect(values).toEqual({});
+        });
+
+        it('should return the values set before', function () {
+            $translateStorage.setValues(en.lang, en.values);
+            $translateStorage.setValues(ru.lang, ru.values);
+
+            var enValues = $translateStorage.getValues(en.lang);
+            var ruValues = $translateStorage.getValues(ru.lang);
+
+            expect(enValues).toEqual(en.values);
+            expect(ruValues).toEqual(ru.values);
+        });
+
+        it('should not change values if called without values', function () {
+            $translateStorage.setValues(en.lang, en.values);
+            $translateStorage.setValues(en.lang);
+            var values = $translateStorage.getValues(en.lang);
+
+            expect(values).toEqual(en.values);
+        });
+
+        it('should use last value if key already exist', function() {
+            $translateStorage.setValues(en.lang, en.values);
+            $translateStorage.setValues(en.lang, en.moreValues);
+            var values = $translateStorage.getValues(en.lang);
+
+            expect(values).toEqual(en.mergedValues);
+        });
+
+        it('should return undefined if called without lang and key', function() {
+            var value = $translateStorage.getValue();
+
+            expect(value).toBeUndefined();
+        });
+
+        it('should return undefined if called without key', function () {
+            $translateStorage.setValues(en.lang, en.values);
+            var value = $translateStorage.getValue(en.lang);
+
+            expect(value).toBeUndefined();
+        });
+
+        it('should return key if don\'t have this key', function () {
+            var value = $translateStorage.getValue(en.lang, en.key);
+
+            expect(value).toEqual(en.key);
+        });
+
+        it('should return value by key', function () {
+            $translateStorage.setValues(en.lang, en.values);
+            var value = $translateStorage.getValue(en.lang, en.key);
+
+            expect(value).toEqual(en.values[en.key]);
+        });
     });
 });
