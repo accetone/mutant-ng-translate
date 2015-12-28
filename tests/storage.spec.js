86d4d113ad2bc462ab10ae236ad2680174a3ec09
Nikita Tsyhanenko <runninzero@gmail.com>
2015-12-28 21:55:49 +0300

    create translate storage test suite with test params initialization

---
+++ tests/storage.spec.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+describe('The translate storage test suite', function () {
+    var $translateStorage, firstLang, secondLang, firstValues, secondValues;
+
+    beforeEach(function () {
+        module('mutant-ng-translate');
+
+        inject(function (_$translateStorage_) {
+            $translateStorage = _$translateStorage_;
+        });
+        
+        firstLang = 'en';
+        secondLang = 'ru';
+
+        firstValues = {
+            'apple': 'Hello, Apple!',
+            'pear': 'Hello, Pear!'
+        };
+
+        secondValues = {
+            'apple': 'Привет, Яблоко!',
+            'pear': 'Привет, Груша!'
+        };
+    });
+});
