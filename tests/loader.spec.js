c289723247c26a0c5bc0751f307e1701825b7d44
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-03 15:19:19 +0300

    refactor translate loader suite data

--- tests/loader.spec.js
+++ tests/loader.spec.js
@@ -1,15 +1,14 @@
 ﻿'use strict';
 
 describe('The translate loader test suite', function() {
-    var $translateLoader, $httpBackend, $http, en, ru, options;
+    var $translateLoader, $httpBackend, en, options;
 
     beforeEach(function() {
         module('mutant-ng-translate');
 
-        inject(function (_$translateLoader_, _$httpBackend_, _$http_) {
+        inject(function (_$translateLoader_, _$httpBackend_) {
             $httpBackend = _$httpBackend_;
             $translateLoader = _$translateLoader_;
-            $http = _$http_;
 
             en = {
                 lang: 'en',
@@ -17,24 +16,9 @@ describe('The translate loader test suite', function() {
                     'apple': 'Hello, Apple!',
                     'pear': 'Hello, Pear!'
                 },
-                moreValues: {
-                    'apple': 'Hello, Apple 2!',
-                    'peach': 'Hello, Peach!'
-                },
-                mergedValues: {
-                    'apple': 'Hello, Apple 2!',
-                    'pear': 'Hello, Pear!',
-                    'peach': 'Hello, Peach!'
-                },
-                key: 'apple',
-                notExistKey: 'lime'
-            };
-
-            ru = {
-                lang: 'ru',
-                values: {
-                    'apple': 'Привет, Яблоко!',
-                    'pear': 'Привет, Груша!'
+                transformedValues: {
+                    'apple': 'Hello, Apple!Hello, Apple!',
+                    'pear': 'Hello, Pear!Hello, Pear!'
                 }
             };
 
