b5342a1f5d0d58ba8b3791c250ae64fac6e795f5
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-04 16:59:20 +0300

    implement common tests for translate cache suite

---
+++ tests/cache.spec.js
@@ -0,0 +1,50 @@
+﻿'use strict';
+
+describe('The translate cache test suite', function() {
+    var $translateCache, en, ru;
+
+    beforeEach(function () {
+        module('mutant-ng-translate');
+
+        inject(function (_$translateCache_) {
+            $translateCache = _$translateCache_;
+        });
+
+        en = {
+            lang: 'en',
+            values: {
+                'apple': 'Hello, Apple!',
+                'pear': 'Hello, Pear!'
+            },
+            moreValues: {
+                'apple': 'Hello, Apple 2!',
+                'peach': 'Hello, Peach!'
+            },
+            mergedValues: {
+                'apple': 'Hello, Apple 2!',
+                'pear': 'Hello, Pear!',
+                'peach': 'Hello, Peach!'
+            },
+            key: 'apple',
+            notExistKey: 'lime'
+        };
+
+        ru = {
+            lang: 'ru',
+            values: {
+                'apple': 'Привет, Яблоко!',
+                'pear': 'Привет, Груша!'
+            }
+        };
+    });
+
+    describe('Common tests', function () {
+        it('should be defined', function () {
+            expect($translateCache).toBeDefined();
+        });
+
+        it('should be an object', function () {
+            expect(typeof $translateCache).toBe('object');
+        });
+    });
+});
