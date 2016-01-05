cef94ac50c4436326a7a1e73d0c709d011075f73
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-05 12:45:36 +0300

    implement common tests for translate events suite

---
+++ tests/events.spec.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+describe('The translate events test suite', function() {
+    var $translateEvents;
+
+    beforeEach(function() {
+        module('mutant-ng-translate');
+
+        inject(function (_$translateEvents_) {
+            $translateEvents = _$translateEvents_;
+        });
+    });
+
+    describe('Common tests', function () {
+        it('should be defined', function () {
+            expect($translateEvents).toBeDefined();
+        });
+
+        it('should be an object', function () {
+            expect(typeof $translateEvents).toBe('object');
+        });
+    });
+});
