442a4623bc50cebd6cce7ceee5a3a3d933462bd6
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-05 13:08:10 +0300

    implement subscribe exceptions tests in translate events suite

--- tests/events.spec.js
+++ tests/events.spec.js
@@ -20,4 +20,16 @@ describe('The translate events test suite', function() {
             expect(typeof $translateEvents).toBe('object');
         });
     });
+
+    describe('Subscribe exceptions tests', function() {
+        it('should throw an exception if called without params', function () {
+            expect($translateEvents.partLoaded.subscribe)
+                .toThrowError(/callback must be defined to subscribe for an event/);
+        });
+
+        it('should throw an exception if callback is not a function', function () {
+            expect(function () { $translateEvents.partLoaded.subscribe('callback'); })
+                .toThrowError(/callback must be a function to subscribe for an event/);
+        });
+    });
 });
