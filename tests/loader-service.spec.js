615f726614ba93f88c6a7b171832d404dc8ff700
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-09 16:59:09 +0300

    implement common tests for translate loader service suite

---
+++ tests/loader-service.spec.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+describe('The translate loader service test suite', function () {
+    var $service, $storage;
+
+    beforeEach(function () {
+        module('mutant-ng-translate');
+
+        inject(function ($translateLoaderSvc, $translateStorage) {
+            $service = $translateLoaderSvc;
+            $storage = $translateStorage;
+        });
+    });
+
+    describe('>> Common tests', function () {
+        it('should be defined', function () {
+            expect($service).toBeDefined();
+        });
+
+        it('should be an object', function () {
+            expect(typeof $service).toBe('object');
+        });
+    });
+
+});
