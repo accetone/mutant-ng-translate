a44d240b234ed41110f5f34e04cb30a5abb341dc
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-11 20:41:50 +0300

    add todo to translate service

--- src/service.js
+++ src/service.js
@@ -144,6 +144,7 @@
         }
 
         function addPart(name) {
+            // TODO: add is string validation 
             var lang = $storage.getLang();
 
             $loader.addPart(name, lang);
