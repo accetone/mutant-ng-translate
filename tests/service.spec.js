3b05391ecbac6aae85e7a2aee0e496cb3976f3dc
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-13 14:10:59 +0300

    implement common tests for translate service suite

---
+++ tests/service.spec.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+describe('The translate service test suite', function () {
+    var $translate;
+
+    beforeEach(function () {
+        module('mutant-ng-translate');
+
+        inject(function (_$translate_) {
+            $translate = _$translate_;
+        });
+    });
+
+    describe('>> Common tests', function () {
+        it('should be defined', function () {
+            expect($translate).toBeDefined();
+        });
+
+        it('should be an object', function () {
+            expect(typeof $translate).toBe('object');
+        });
+    });
+});
