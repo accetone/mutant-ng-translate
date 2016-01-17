8447b9290c045b282d43c5d7d88277100b06647d
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-17 16:14:14 +0300

    implement transformation tests for translate filter suite

--- tests/filter.spec.js
+++ tests/filter.spec.js
@@ -6,6 +6,8 @@ describe('The translate filter test suite', function () {
     beforeEach(function() {
         module('mutant-ng-translate');
 
+        window.localStorage.clear();
+
         inject(function (_$translate_, _$filter_) {
             $translate = _$translate_;
             $filter = _$filter_('translate');
@@ -15,8 +17,6 @@ describe('The translate filter test suite', function () {
             defaultLang: 'en',
             urlTemplate: '/locale-{part}-{lang}.json'
         };
-
-        $translate.config(options);
     });
 
     describe('>> Common tests', function () {
@@ -26,6 +26,25 @@ describe('The translate filter test suite', function () {
 
         it('should be an function', function () {
             expect(typeof $filter).toBe('function');
+        });
+    });
+
+    describe('>> Transformation tests', function() {
+        beforeEach(function() {
+            $translate.config(options);
+            $translate.translations('en', { test: 'test-test' });
+        });
+
+        it('should return value for existing key', function() {
+            var value = $filter('test');
+
+            expect(value).toEqual('test-test');
+        });
+
+        it('should return key for non existing key', function () {
+            var value = $filter('test2');
+
+            expect(value).toEqual('test2');
         });
     });
 });
