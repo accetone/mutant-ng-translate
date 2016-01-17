80aea14dd473239f351b448f138b0a163cd5142e
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-17 17:13:14 +0300

    implement translate directive test suite

---
+++ tests/directive.spec.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+
+describe('The translate directive test suite', function () {
+    var $compile, $rootScope, $translate, options;
+
+    beforeEach(function () {
+        module('mutant-ng-translate');
+
+        window.localStorage.clear();
+
+        inject(function (_$translate_, _$compile_, _$rootScope_) {
+            $translate = _$translate_;
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
+        });
+
+        options = {
+            defaultLang: 'en',
+            urlTemplate: '/locale-{part}-{lang}.json'
+        };
+
+        $translate.config(options);
+
+        $translate.translations('en', { test: 'test-test' });
+    });
+
+    describe('>> Attribute tests', function() {
+        it('should contain value for existing key', function() {
+            var element = $compile('<div ng-translate="test"></div>')($rootScope);
+
+            expect(element.html()).toContain('test-test');
+        });
+
+        it('should contain key for non existing key', function () {
+            var element = $compile('<div ng-translate="test2"></div>')($rootScope);
+
+            expect(element.html()).toContain('test2');
+        });
+    });
+});
