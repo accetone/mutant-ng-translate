8e50ef50fbea5428976146bde94b3698f2f66aaf
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-17 15:16:47 +0300

    implement common tests for translate filter suite

---
+++ tests/filter.spec.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+
+describe('The translate filter test suite', function () {
+    var $filter, $translate, options;
+
+    beforeEach(function() {
+        module('mutant-ng-translate');
+
+        inject(function (_$translate_, _$filter_) {
+            $translate = _$translate_;
+            $filter = _$filter_('translate');
+        });
+
+        options = {
+            defaultLang: 'en',
+            urlTemplate: '/locale-{part}-{lang}.json'
+        };
+
+        $translate.config(options);
+    });
+
+    describe('>> Common tests', function () {
+        it('should be defined', function () {
+            expect($filter).toBeDefined();
+        });
+
+        it('should be an function', function () {
+            expect(typeof $filter).toBe('function');
+        });
+    });
+});
