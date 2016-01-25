552f1bf0ea8a9d285c5b5a7cd56c260e97ac8de2
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-25 16:37:38 +0300

    docs for translate directive

--- src/directive.js
+++ src/directive.js
@@ -5,6 +5,20 @@
         .module('mutant-ng-translate')
         .directive('ngTranslate', ['$translate', '$translateEvents', translateDirective]);
 
+    /**
+     * @ngdoc directive
+     * @name translate.directive:ngTranslate
+     * @restrict 'A'
+     * @element ANY
+     * @requires translate.$translate
+     * @requires translate.events
+     * 
+     * @param {string} ngTranslate Translation key
+     * 
+     * @description 
+     * Transform any html element to the translation container. 
+     * Translations get from {@link translate.$translate $translate} service by specified key
+     */
     function translateDirective($translate, $translateEvents) {
         var link = function (scope, element, attrs) {
             element = element[0];
