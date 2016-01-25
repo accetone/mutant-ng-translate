903400ce37f10174a63a68d24f688fd88ed5dce7
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-25 16:38:08 +0300

    docs for translate filter

--- src/filter.js
+++ src/filter.js
@@ -5,6 +5,17 @@
         .module('mutant-ng-translate')
         .filter('translate', ['$translate', translateFilter]);
 
+    /**
+     * @ngdoc filter
+     * @name translate.filter:translate
+     * @function
+     * @requires translate.$translate
+     * 
+     * @param {string} translate_expression Translation key
+     * 
+     * @description 
+     * Transform translation key into translation
+     */
     function translateFilter($translate) {
         var filter = function(key) {
             return $translate.translation(key);
