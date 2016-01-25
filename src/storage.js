54c5b0b96d1883e59e2eb0a4351afb7ff2283c6d
Nikita Tsyhanenko <runninzero@gmail.com>
2016-01-25 16:37:22 +0300

    docs for translate storage

--- src/storage.js
+++ src/storage.js
@@ -1,10 +1,17 @@
-﻿(function() {
+﻿(function () {
     'use strict';
 
     angular
         .module('mutant-ng-translate')
         .factory('$translateStorage', [translateStorage]);
 
+    /**
+     * @ngdoc service
+     * @name translate.storage
+     * 
+     * @description 
+     * Service responsible for storing the translations data while app is running
+     */
     function translateStorage() {
         var self = this;
 
@@ -18,6 +25,17 @@
 
         return self;
 
+        /**
+         * @ngdoc method
+         * @methodOf translate.storage
+         * @name getValues
+         * 
+         * @param {string} lang Language
+         * @returns {Object.<string, string>} Values Hashmap
+         * 
+         * @description 
+         * Return values hashmap for given language
+         */
         function getValues(lang) {
             if (!self.exists(lang)) {
                 self.langs[lang] = {};
@@ -26,6 +44,18 @@
             return self.langs[lang];
         }
 
+        /**
+         * @ngdoc method
+         * @methodOf translate.storage
+         * @name getValue
+         * 
+         * @param {string} lang Language
+         * @param {string} key Key
+         * @returns {string} Value
+         * 
+         * @description 
+         * Return value by key from language hashmap
+         */
         function getValue(lang, key, resolver) {
             if (!self.exists(lang, key)) {
                 if (typeof resolver === 'function') {
@@ -38,6 +68,18 @@
             return self.langs[lang][key];
         }
 
+        /**
+         * @ngdoc method
+         * @methodOf translate.storage
+         * @name setValues
+         * 
+         * @param {string} lang Language
+         * @param {Object.<string, string>} values Values Hashmap
+         * 
+         * @description 
+         * Merge existing hashmap for given language with passed hashmap. 
+         * If hashmap not exists, will create empty object first
+         */
         function setValues(lang, values) {
             if (!self.langs.hasOwnProperty(lang)) {
                 self.langs[lang] = {};
@@ -46,6 +88,18 @@
             angular.merge(self.langs[lang], values);
         }
 
+        /**
+         * @ngdoc method
+         * @methodOf translate.storage
+         * @name exists
+         * 
+         * @param {string} lang Language
+         * @param {string} key Key
+         * @returns {boolean} Is Exists
+         * 
+         * @description 
+         * Check if key exists in language hashmap 
+         */
         function exists(lang, key) {
             if (!self.langs.hasOwnProperty(lang)) return false;
 
